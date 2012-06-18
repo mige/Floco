a35fe119e674631fb75510cbd6460fc0e5d4747c
Aleš Nejdr <ales.nejdr@gmail.com>
2012-06-18 12:40:13 +0200

    Changed the view of data.

--- src/models/tournamentmodel.cpp
+++ src/models/tournamentmodel.cpp
@@ -12,3 +12,27 @@ TournamentModel::TournamentModel(QObject *parent) :
     setHeaderData(2, Qt::Horizontal, QObject::tr("Place"));
     setHeaderData(3, Qt::Horizontal, QObject::tr("In season"));
 }
+
+/**
+ * @brief Return data
+ * @param index Model index
+ * @param role Role
+ * @return Data
+ */
+QVariant TournamentModel::data(const QModelIndex &index, int role) const
+{
+    if(!index.isValid()) return QVariant();
+
+    QVariant value = QSqlTableModel::data(index, role);
+
+    if(role == Qt::DisplayRole)
+    {
+        if(index.column() == 3)
+        {
+            bool isChecked = value.toBool();
+            return isChecked ? QVariant(tr("In season")) : QVariant("Off season");
+        }
+    }
+
+    return value;
+}
--- src/models/tournamentmodel.h
+++ src/models/tournamentmodel.h
@@ -8,6 +8,7 @@ class TournamentModel : public QSqlTableModel
     Q_OBJECT
 public:
     explicit TournamentModel(QObject *parent = 0);
+    QVariant data(const QModelIndex &idx, int role) const;
     
 signals:
     
